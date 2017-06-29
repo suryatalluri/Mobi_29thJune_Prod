d6663f1b90859feee8652390a40c59d162c6669b
root <root@40ee80ac6db9>
2017-06-29 04:50:03 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobi 29thJune Prod",
+  "homePage" : "Main",
+  "name" : "Mobi_29thJune_Prod",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
